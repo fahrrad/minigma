e249ef19bdf10596d7586aa901b886200d8e1014
Ward Coessens <ward.coessens@gmail.com>
2019-08-18 22:19:10 +0200

    Switch to I2C io expander for GPIO

--- read_6_buttons/read_6_buttons.ino
+++ read_6_buttons/read_6_buttons.ino
@@ -1,42 +1,57 @@
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+
+const int16_t I2C_SLAVE = 0x20;
+
 bool readColumnOne;
 bool readColumnTwo;
 
 // GPIO pins for LED Rows
 int LED_NUMBER_OF_ROWS=3;
-int ledRowPins[3] = {D0, D2, D3};
+//int ledRowPins[3] = {D0, D2, D3};
+int ledRowPins[3] = {3,4,5};
 int LED_NUMBER_OF_COLUMNS=2;
-int ledColumnPins[2] = {D1, D4};
+//int ledColumnPins[2] = {D1, D4};
+int ledColumnPins[2] = {1,2};
 
 // GPIO pins for Keys
 int KEY_NUMBER_OF_ROWS = 3;
-int keyRowPins[3] = {D5, D8, 10};
+// int keyRowPins[3] = {D5, D8, 9};
+ int keyRowPins[3] = {6, 7, 8};
 int KEY_NUMBER_OF_COLUMNS = 2;
-int keyColumnPins[2] = {D6, D7};
+// int keyColumnPins[2] = {D6, D7};
+ int keyColumnPins[2] = {8, 9};
 
+Adafruit_MCP23017 mcp;
 
 void setup() {
   Serial.begin(9600);
+  mcp.begin(I2C_SLAVE); 
+  mcp.pinMode(7, OUTPUT); 
+  mcp.pinMode(7, OUTPUT); 
+  mcp.digitalWrite(6, LOW);
+  
   // Setup GPIO pins for keys
   // these 2 pins will be read. (columns)
   // They should be pulled down.
-  for(int i = 0; i < LED_NUMBER_OF_COLUMNS ; i++ ){
-    pinMode(keyColumnPins[i], INPUT);
-  }  
-
-  // Rows will be scanned.
-  for(int i = 0; i < KEY_NUMBER_OF_ROWS ; i++ ){
-    pinMode(keyRowPins[i], OUTPUT);
-  }  
-  
-  // Setup LED GPIOs for output
-  // Rows
-  for(int i=0; i < 3; i++){
-    pinMode(ledRowPins[i], OUTPUT);
-  }
-  // columns
-  for(int i=0; i < 2; i++){
-    pinMode(ledColumnPins[i], OUTPUT);
-  }
+//  for(int i = 0; i < LED_NUMBER_OF_COLUMNS ; i++ ){
+//    pinMode(keyColumnPins[i], INPUT);
+//  }  
+//
+//  // Rows will be scanned.
+//  for(int i = 0; i < KEY_NUMBER_OF_ROWS ; i++ ){
+//    pinMode(keyRowPins[i], OUTPUT);
+//  }  
+//  
+//  // Setup LED GPIOs for output
+//  // Rows
+//  for(int i=0; i < 3; i++){
+//    pinMode(ledRowPins[i], OUTPUT);
+//  }
+//  // columns
+//  for(int i=0; i < 2; i++){
+//    pinMode(ledColumnPins[i], OUTPUT);
+//  }
 }
 
 void turnAllLedsOff(){
@@ -74,27 +89,23 @@ void loop_(){
   }
 }
 
-void loop() {
+void loop__() {
   // Rows
   int keyRead = -1;
   for (int r = 0; r < KEY_NUMBER_OF_ROWS; r++){
     // Scan 1 Row
-    Serial.println("Set ROW " + String(keyRowPins[r]) + " HIGH");
+    //Serial.println("Set ROW " + String(keyRowPins[r]) + " HIGH");
     digitalWrite(keyRowPins[r], HIGH);
     for(int c = 0; c < KEY_NUMBER_OF_COLUMNS; c++){
-      delay(100);
       bool cRead = digitalRead(keyColumnPins[c] );
-      Serial.println("Read COLUMNS " + String(keyColumnPins[c]) + ": " + String(cRead));
+      // Serial.println("Read COLUMNS " + String(keyColumnPins[c]) + ": " + String(cRead));
       if (cRead){
-        keyRead = ((r+1) * KEY_NUMBER_OF_ROWS) + c;
-        delay(100);
+        keyRead = (r * KEY_NUMBER_OF_ROWS) + c;
         Serial.println("Read key " + String(keyRead));
       }
     }
-    Serial.println("Set ROW " + String(keyRowPins[r]) + " LOW");
+    // Serial.println("Set ROW " + String(keyRowPins[r]) + " LOW");
     digitalWrite(keyRowPins[r], LOW);
-
-    delay(1000);
   }
 
   if (keyRead != -1 ) 
@@ -102,36 +113,13 @@ void loop() {
     turnOnLed100ms(keyRead);  
     keyRead = -1; 
   }
-  
-//  digitalWrite(8, HIGH);
-//  digitalWrite(9, LOW);
-//  digitalWrite(10, LOW);
-//
-//  readColumnOne = digitalRead(2);
-//  readColumnTwo = digitalRead(3);
-//
-//  if (readColumnOne) turnOnLed100ms(0); 
-//  if (readColumnTwo) turnOnLed100ms(1);
-//
-//  // Row 2
-//  digitalWrite(8, LOW);
-//  digitalWrite(9, HIGH);
-//  digitalWrite(10, LOW);
-//
-//  readColumnOne = digitalRead(2);
-//  readColumnTwo = digitalRead(3);
-//
-//  if (readColumnOne) turnOnLed100ms(2);
-//  if (readColumnTwo) turnOnLed100ms(3);
-//
-//  // Row 3
-//  digitalWrite(8, LOW);
-//  digitalWrite(9, LOW);
-//  digitalWrite(10, HIGH);
-//
-//  readColumnOne = digitalRead(2);
-//  readColumnTwo = digitalRead(3);
-//
-//  if (readColumnOne) turnOnLed100ms(4);
-//  if (readColumnTwo) turnOnLed100ms(5);
+}
+
+void loop(){
+  Serial.println("Setting 7 HIGH");
+  mcp.digitalWrite(7, HIGH);
+  delay(300);
+//  
+  mcp.digitalWrite(7, LOW);
+  delay(300);
 }
