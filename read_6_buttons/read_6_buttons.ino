fd5154bd0eb7bbbe8a7ccdbbd636da2a7a1fa4e4
Ward Coessens <ward.coessens@gmail.com>
2019-08-18 14:34:41 +0200

    trying to add keys for ESP8266

--- read_6_buttons/read_6_buttons.ino
+++ read_6_buttons/read_6_buttons.ino
@@ -3,24 +3,33 @@ bool readColumnTwo;
 
 // GPIO pins for LED Rows
 int LED_NUMBER_OF_ROWS=3;
-int ledRowPins[3] = {D0, D3, D5};
+int ledRowPins[3] = {D0, D2, D3};
 int LED_NUMBER_OF_COLUMNS=2;
 int ledColumnPins[2] = {D1, D4};
+
+// GPIO pins for Keys
+int KEY_NUMBER_OF_ROWS = 3;
+int keyRowPins[3] = {D5, D8, 10};
+int KEY_NUMBER_OF_COLUMNS = 2;
+int keyColumnPins[2] = {D6, D7};
+
 
 void setup() {
   Serial.begin(9600);
   // Setup GPIO pins for keys
-  // these 2 pins will be read. 
+  // these 2 pins will be read. (columns)
   // They should be pulled down.
-//  pinMode(, INPUT);
-//  pinMode(, INPUT);
-//  
-//  pinMode(, OUTPUT);
-//  pinMode(, OUTPUT);
-//  pinMode(, OUTPUT);
+  for(int i = 0; i < LED_NUMBER_OF_COLUMNS ; i++ ){
+    pinMode(keyColumnPins[i], INPUT);
+  }  
 
-// Setup LED GPIOs for output
-// Rows
+  // Rows will be scanned.
+  for(int i = 0; i < KEY_NUMBER_OF_ROWS ; i++ ){
+    pinMode(keyRowPins[i], OUTPUT);
+  }  
+  
+  // Setup LED GPIOs for output
+  // Rows
   for(int i=0; i < 3; i++){
     pinMode(ledRowPins[i], OUTPUT);
   }
@@ -44,7 +53,7 @@ void turnAllLedsOff(){
   }
 }
 
-void turnOnLed1Sec(int number){
+void turnOnLed100ms(int number){
   int column = number % LED_NUMBER_OF_COLUMNS;
   int row = number / LED_NUMBER_OF_COLUMNS;
  
@@ -57,45 +66,72 @@ void turnOnLed1Sec(int number){
 
 }
 
-void loop(){
+void loop_(){
   for (int i =0; i< 6; i++){
-    Serial.println("switching on" +  String(i));
-    turnOnLed1Sec(i);
+    //Serial.println("switching on" +  String(i));
+    turnOnLed100ms(i);
     delay(1000);
   }
 }
 
-void loop_() {
-  // Row 1
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
+void loop() {
+  // Rows
+  int keyRead = -1;
+  for (int r = 0; r < KEY_NUMBER_OF_ROWS; r++){
+    // Scan 1 Row
+    Serial.println("Set ROW " + String(keyRowPins[r]) + " HIGH");
+    digitalWrite(keyRowPins[r], HIGH);
+    for(int c = 0; c < KEY_NUMBER_OF_COLUMNS; c++){
+      delay(100);
+      bool cRead = digitalRead(keyColumnPins[c] );
+      Serial.println("Read COLUMNS " + String(keyColumnPins[c]) + ": " + String(cRead));
+      if (cRead){
+        keyRead = ((r+1) * KEY_NUMBER_OF_ROWS) + c;
+        delay(100);
+        Serial.println("Read key " + String(keyRead));
+      }
+    }
+    Serial.println("Set ROW " + String(keyRowPins[r]) + " LOW");
+    digitalWrite(keyRowPins[r], LOW);
 
-  readColumnOne = digitalRead(2);
-  readColumnTwo = digitalRead(3);
+    delay(1000);
+  }
 
-  if (readColumnOne) turnOnLed1Sec(0); 
-  if (readColumnTwo) turnOnLed1Sec(1);
-
-  // Row 2
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-
-  readColumnOne = digitalRead(2);
-  readColumnTwo = digitalRead(3);
-
-  if (readColumnOne) turnOnLed1Sec(2);
-  if (readColumnTwo) turnOnLed1Sec(3);
-
-  // Row 3
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-
-  readColumnOne = digitalRead(2);
-  readColumnTwo = digitalRead(3);
-
-  if (readColumnOne) turnOnLed1Sec(4);
-  if (readColumnTwo) turnOnLed1Sec(5);
+  if (keyRead != -1 ) 
+  {
+    turnOnLed100ms(keyRead);  
+    keyRead = -1; 
+  }
+  
+//  digitalWrite(8, HIGH);
+//  digitalWrite(9, LOW);
+//  digitalWrite(10, LOW);
+//
+//  readColumnOne = digitalRead(2);
+//  readColumnTwo = digitalRead(3);
+//
+//  if (readColumnOne) turnOnLed100ms(0); 
+//  if (readColumnTwo) turnOnLed100ms(1);
+//
+//  // Row 2
+//  digitalWrite(8, LOW);
+//  digitalWrite(9, HIGH);
+//  digitalWrite(10, LOW);
+//
+//  readColumnOne = digitalRead(2);
+//  readColumnTwo = digitalRead(3);
+//
+//  if (readColumnOne) turnOnLed100ms(2);
+//  if (readColumnTwo) turnOnLed100ms(3);
+//
+//  // Row 3
+//  digitalWrite(8, LOW);
+//  digitalWrite(9, LOW);
+//  digitalWrite(10, HIGH);
+//
+//  readColumnOne = digitalRead(2);
+//  readColumnTwo = digitalRead(3);
+//
+//  if (readColumnOne) turnOnLed100ms(4);
+//  if (readColumnTwo) turnOnLed100ms(5);
 }
