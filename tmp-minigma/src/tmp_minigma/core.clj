f96e6081bad8276c4242fb573fbe22faa8288070
fahhrad <ward.coessens@gmail.com>
2019-10-16 07:47:32 +0200

    trying to get to symetric encryption

--- tmp-minigma/src/tmp_minigma/core.clj
+++ tmp-minigma/src/tmp_minigma/core.clj
@@ -1,22 +1,57 @@
 (ns tmp-minigma.core
   (:require [clojure.test :refer :all]))
 
+(def rotor-position-1 (atom 0))
+(def rotor-position-2 (atom 0))
+(def rotor-position-3 (atom 0))
+
+(def rotor-1 "ekmflgdqvzntowyhxuspaibrcj")
+(def rotor-2 "ajdksiruxblhwtmcqgznpyfvoe")
+(def rotor-3 "bdfhjlcprtxvznyeiwgakmusqo")
+(def reflector "ejmzalyxvbwfcrquontspikhgd")
+
 (defn ord [c]
   (- (int c) (int \a)))
 
+(defn dro [n]
+  (char (+ n (ord \a))))
+
+(defn alfa [c]
+  (and (char? c) (<= (ord \a) (ord c) (ord \z))))
+
 (defn rotate [c rotation]
-  (let [i (ord c)
+  {:pre [(alfa c)]
+   :post [(alfa c)]}
+#dbg  (let [i (ord c)
         r (mod rotation 26)
         n (+ (int \a) (mod (+ i r) 26))]
     (char n)))
 
-(defn translate [i translation]
-  (.charAt translation (ord i)))
+(defn translate [c translation]
+  {:pre [(alfa c)]
+   :post ([alfa %])}
+  #dbg (.charAt translation (ord c)))
 
 (defn rotors [n]
-  (let [r1 (mod n (pow 26 3))]))
+  (let [r1 (mod n (Math/pow 26 3))]))
 
-(defn enigmate [])
+(defn enigmate [c]
+  (-> c
+      (rotate @rotor-position-1)
+      (translate rotor-1)
+      ;; (rotate @rotor-position-2)
+      ;; (translate rotor-2)
+      ;; (rotate @rotor-position-3)
+      ;; (translate rotor-3)
+
+      (translate reflector)
+
+      ;; (rotate @rotor-position-3)
+      ;; (translate rotor-3)
+      ;; (rotate @rotor-position-2)
+      ;; (translate rotor-2)
+      (rotate @rotor-position-1)
+      (translate rotor-1)))
 
 (deftest rotate-test
   (testing "rotate by 0 is the same"
@@ -38,3 +73,7 @@
   (testing "1 is [1 1 1]")
   (testing "2 is [1 1 2]")
   (testing "26 is [1 1 26]"))
+
+(deftest enigmate-test
+  (testing "encrption should be symetric"
+    (is (= \a (enigmate (enigmate \a))))))
